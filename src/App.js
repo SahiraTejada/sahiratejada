407707290357056781c0eb667670f9ce3d7b337d
SahiraTejada <Sahira2208@hotmail.com>
2023-03-22 22:36:25 -0400

    Create Contact

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Banner from './components/Banner';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Banner/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   );
 }
